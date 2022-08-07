14948967bc6f49d8d61f10d1cb25e5bd45480bb8
ginabeki <ginabeki3@gmail.com>
2022-08-07 21:25:40 +0300

    update style

---
+++ script.js
@@ -0,0 +1,95 @@
+const form = document.getElementById("form");
+const owner = document.getElementById("owner");
+const card = document.getElementById("card-number");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const cvc = document.getElementById("cvc");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // trim to remove whitespaces
+  const ownerValue = owner.value.trim();
+  const cardValue = card.value.trim();
+  const yearValue = year.value.trim();
+  const monthValue = month.value.trim();
+  const cvcValue = cvc.value.trim();
+
+  if (ownerValue === "") {
+    setErrorFor(owner, "Can't be blank");
+  } else {
+    setSuccessFor(owner);
+  }
+  if (cardValue === "") {
+    setErrorFor(card, "Can't be blank");
+  } else if (!isCardNUmber(cardValue)) {
+    setErrorFor(card, "Wrong format, number only");
+  } else {
+    setSuccessFor(card);
+  }
+
+  if (yearValue === "") {
+    setErrorFor(year, "Can't be blank");
+  } else if(!isYear(yearValue)) {
+    setErrorFor(year, "Invalid year format")
+  } else {
+    setSuccessFor(year);
+  }
+
+  if (monthValue === "") {
+    setErrorFor(month, "Can't be blank");
+  } else if(!isMonth(monthValue)) {
+    setErrorFor(month, "Invalid month format");
+  } else {
+    setSuccessFor(month);
+  }
+
+  if (cvcValue === "") {
+    setErrorFor(cvc, "Can't be blank");
+  } else if (!isCvc(cvcValue)) {
+    setErrorFor(cvc, "Length must be 4");
+  } else {
+    setSuccessFor(cvc);
+  }
+
+  // show success message on confirmation. Thank you for submission
+  if (ownerValue && cardValue && yearValue && monthValue && cvcValue === true ) {
+    setSuccessFor(alert("yeeeeeiy"))
+  } else {
+    return false;
+  }
+
+};
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; //.form-control
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+function setSuccessFor(input, message) {
+  const formControl = input.parentElement; //.form-control
+  formControl.className = "form-control success";
+}
+
+function isCardNUmber(card) {
+  let number = /^[0-9]{12}$/;
+  return number.test(card);
+};
+
+function isYear(year) {
+    let yearNumber = /(^[1-9]$)|(^[0-1][1-2]$)|(^0[1-9]$)/;
+    return yearNumber.test(year);
+}
+
+function isMonth(month) {
+    let monthNumber = /(^[1-9]$)|(^[0-1][1-2]$)|(^0[1-9]$)/;
+    return monthNumber.test(month);
+}
+function isCvc(cvc) {
+    let digits = /^[0-9]{4}$/;
+    return digits.test(cvc);
+}
