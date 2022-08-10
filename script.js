2a81b9b43abeda716dcd5a68768d1d95ef832ebb
ginabeki <ginabeki3@gmail.com>
2022-08-10 22:02:03 +0300

    refactor code to improve readability

--- script.js
+++ script.js
@@ -7,20 +7,25 @@ const cvc = document.getElementById("cvc");
 const completed = document.querySelector(".complete");
 const backHome = document.querySelector(".complete button");
 
-document.querySelector(".owner").oninput = () =>{
-    document.querySelector(".name").innerText = document.querySelector(".owner").value;
+document.querySelector(".owner").oninput = () => {
+  document.querySelector(".name").innerText =
+    document.querySelector(".owner").value;
 };
-document.querySelector(".card-input").oninput = () =>{
-    document.querySelector(".span-1").innerText = document.querySelector(".card-input").value;
+document.querySelector(".card-input").oninput = () => {
+  document.querySelector(".span-1").innerText =
+    document.querySelector(".card-input").value;
 };
 document.querySelector(".month").oninput = () => {
-    document.querySelector(".expiry-date").innerText = document.querySelector(".month").value;
+  document.querySelector(".expiry-date").innerText =
+    document.querySelector(".month").value;
 };
 document.querySelector(".year").oninput = () => {
-    document.querySelector(".exp-year").innerText = document.querySelector(".year").value;
+  document.querySelector(".exp-year").innerText =
+    document.querySelector(".year").value;
 };
 document.querySelector(".cvc").oninput = () => {
-    document.querySelector(".text").innerText = document.querySelector(".cvc").value;
+  document.querySelector(".text").innerText =
+    document.querySelector(".cvc").value;
 };
 
 form.addEventListener("submit", (e) => {
@@ -44,23 +49,23 @@ function checkInputs() {
   }
   if (cardValue === "") {
     setErrorFor(card, "Can't be blank");
-  } else if (!isCardNUmber(cardValue)) {
-    setErrorFor(card, "Wrong format, number only");
+  } else if (!isCardNumber(cardValue)) {
+    setErrorFor(card, "Length must be 16");
   } else {
     setSuccessFor(card);
   }
 
   if (yearValue === "") {
     setErrorFor(year, "Can't be blank");
-  } else if(!isYear(yearValue)) {
-    setErrorFor(year, "Invalid format")
+  } else if (!isYear(yearValue)) {
+    setErrorFor(year, "Invalid format");
   } else {
     setSuccessFor(year);
   }
 
   if (monthValue === "") {
     setErrorFor(month, "Can't be blank");
-  } else if(!isMonth(monthValue)) {
+  } else if (!isMonth(monthValue)) {
     setErrorFor(month, "Invalid format");
   } else {
     setSuccessFor(month);
@@ -69,51 +74,48 @@ function checkInputs() {
   if (cvcValue === "") {
     setErrorFor(cvc, "Can't be blank");
   } else if (!isCvc(cvcValue)) {
-    setErrorFor(cvc, "Length must be 4");
+    setErrorFor(cvc, "Length must be 3-4");
   } else {
     setSuccessFor(cvc);
   }
 
   // completed
-  if (ownerValue && cardValue && yearValue && monthValue && cvcValue === setSuccessFor ) {
-    form.style.display="none";
-    completed.style.display="block";
-  } else {
-    return setErrorFor;
+  if (ownerValue && cardValue && yearValue && monthValue && cvcValue) {
+    form.style.display = "none";
+    completed.style.display = "block";
   }
-};
+}
 
-
-backHome.addEventListener("click", function(){
+backHome.addEventListener("click", function () {
   window.location.reload();
 });
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement; //.form-control
-	const small = formControl.querySelector('small');
-	formControl.className = 'form-control error';
-	small.innerText = message;
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
 }
 function setSuccessFor(input, message) {
   const formControl = input.parentElement; //.form-control
   formControl.className = "form-control success";
 }
 
-function isCardNUmber(card) {
-  let number = /^[0-9]{12}$/;
+function isCardNumber(card) {
+  let number = /^[0-9]{16}$/;
   return number.test(card);
-};
+}
 
 function isYear(year) {
-    let yearNumber = /[0-2][0-5]|[1-9]/g ;
-    return yearNumber.test(year);
+  let yearNumber = /^[0-9]{2}$/g;
+  return yearNumber.test(year);
 }
 
 function isMonth(month) {
-    let monthNumber = /(^[1-9]$)|(^[0-1][1-2]$)|(^0[1-9]$)/;
-    return monthNumber.test(month);
+  let monthNumber = /^(0[1-9]|1[0-2])/;
+  return monthNumber.test(month);
 }
 function isCvc(cvc) {
-    let digits = /^[0-9]{4}$/;
-    return digits.test(cvc);
+  let digits = /^[0-9]{3,4}$/;
+  return digits.test(cvc);
 }
